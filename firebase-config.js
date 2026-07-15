c26daf29e4609688b6f7f4da58f37ce3fe9c5e9e
hiimmihait <128229010+hiimmihait@users.noreply.github.com>
2026-07-15 15:12:44 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,4 +6,5 @@ const firebaseConfig = {
   messagingSenderId: "125492438064",
   appId: "1:125492438064:web:dbbc65206e0f0004a9f9f5",
   measurementId: "G-T5QECT9VXH"
+  recaptchaSiteKey = "6LePFVUtAAAAAO-racxeADKKSqVuF_G5bmgRurz4"
 };
