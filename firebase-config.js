76a57ff62e241b357575c3aa85907f655bff045f
hiimmihait <128229010+hiimmihait@users.noreply.github.com>
2026-07-15 10:19:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,5 +4,6 @@ const firebaseConfig = {
   projectId: "bugetapp-9a017",
   storageBucket: "bugetapp-9a017.firebasestorage.app",
   messagingSenderId: "125492438064",
-  appId: "1:125492438064:web:dbbc65206e0f0004a9f9f5"
+  appId: "1:125492438064:web:dbbc65206e0f0004a9f9f5",
+  measurementId: "G-T5QECT9VXH"
 };
